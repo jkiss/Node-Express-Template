685f02ae139fdb23d0c3d3c7297d727290ef0bf8
MrB <itjkiss@gmail.com>
2022-03-10 21:01:44 +0800

    mini

--- bash/deploy.sh
+++ bash/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-WEB_PATH='/home/user/project'
-WEB_USER='user'
-WEB_USERGROUP='user'
+# WEB_PATH='/home/user/project'
+# WEB_USER='user'
+# WEB_USERGROUP='user'
 
-echo "Start deployment"
-cd $WEB_PATH
+# echo "Start deployment"
+# cd $WEB_PATH
 
-echo "Adding id_rsa"
-eval "$(ssh-agent -s)"
-ssh-add /home/user/.ssh/id_rsa
+# echo "Adding id_rsa"
+# eval "$(ssh-agent -s)"
+# ssh-add /home/user/.ssh/id_rsa
 
-echo "Pulling source code..."
-git pull
+# echo "Pulling source code..."
+# git pull
 
 # echo "Changing permissions..."
 # sudo chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
